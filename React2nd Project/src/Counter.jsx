0de101fffa210d10ff07e096669b0d061594fdf9
Patel Hani <patelhanii14@gmail.com>
2024-11-30 12:04:13 +0530

    Counter

---
+++ React2nd Project/src/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+const Counter = () => {
+  const [count, setCount] = useState(0);  
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(Math.max(count - 1, 0));
+  const Reset = () => {
+    useCounter(0)
+  }
+
+  return (
+    <div className="counter-container">
+    <h2 style={{color:'black'}}>Counter:</h2>
+      <h1 className="counter-display">{count}</h1>
+      <div className="button-container">
+        <button className="button" onClick={increment}>Increment</button>
+        <button className="button" onClick={decrement}>Decrement</button>
+        <button className="button" onClick={Reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
