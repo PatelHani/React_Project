ce28ccb0a716d35087b8251c078d14f873149747
Patel Hani <patelhanii14@gmail.com>
2024-12-19 18:27:59 +0530

    hospital management

---
+++ React6th-Project/hospital management/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import PatientForm from './Components/PatientForm';
+
+
+
+  const App = () => {
+    return (
+      <div>
+        <PatientForm />
+      </div>
+    );
+  };
+
+  export default App;
